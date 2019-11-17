1e8417aaed9f0b78789f17380077ad7d32e01484
bkavur <kavur.bojan@gmail.com>
2019-11-17 12:29:25 +0100

    Dodana mogućnost odabira datuma. #37

--- myBook/app/src/main/java/com/air/foi/hr/mybook/RegisterActivity.java
+++ myBook/app/src/main/java/com/air/foi/hr/mybook/RegisterActivity.java
@@ -2,13 +2,61 @@ package com.air.foi.hr.mybook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = "RegisterActivity";
+
+    private EditText displayDate;
+    DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        displayDate = findViewById(R.id.birth_date);
+        displayDate.setInputType(InputType.TYPE_NULL);
+
+        displayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                int month = cal.get(Calendar.MONTH);
+                int year = cal.get(Calendar.YEAR);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        RegisterActivity.this,
+                        android.R.style.Theme_DeviceDefault_Light_Panel,
+                        mDateSetListener,
+                        year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FDF7F9")));
+                datePickerDialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month +1;
+                Log.d(TAG, "onDateSet: date dd/mm/yyyy: " + dayOfMonth + "/" + month + "/" + year);
+
+                String date = dayOfMonth + "/" + month + "/" + year;
+                displayDate.setText(date);
+
+            }
+        };
     }
 }
