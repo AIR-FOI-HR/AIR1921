a6d74f1f98e328ef810bab058ed46ba03db7655c
Ana Guštek <ana.gustek@gmail.com>
2019-12-03 22:04:38 +0100

    Postavljeno prikazivanje naslova na toolbaru. #7

--- myBook/app/src/main/java/com/air/foi/hr/mybook/AnketaActivity.java
+++ myBook/app/src/main/java/com/air/foi/hr/mybook/AnketaActivity.java
@@ -1,6 +1,7 @@
 package com.air.foi.hr.mybook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,7 @@ public class AnketaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anketa);
+        Toolbar toolbar = findViewById(R.id.toolbarAnketa);
+        setSupportActionBar(toolbar);
     }
 }
