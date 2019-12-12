ec502f001fd84a6815823b1331100e11eecc03a0
Ana Guštek <ana.gustek@gmail.com>
2019-12-12 22:09:39 +0100

    Kreiran novi activity za prikaz preporučenih knjiga. #47

---
+++ myBook/app/src/main/java/com/air/foi/hr/mybook/PreporuceneKnjigeActivity.java
@@ -0,0 +1,14 @@
+package com.air.foi.hr.mybook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreporuceneKnjigeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preporucene_knjige);
+    }
+}
