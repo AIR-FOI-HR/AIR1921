e4135729433196e5b80d1d73014abb809d248116
Ana Guštek <ana.gustek@gmail.com>
2019-12-13 20:27:19 +0100

    Postavljeno da se na formi za prikaz preporučenih knjiga prikazuje tab "Prijedlozi". #47

--- myBook/app/src/main/java/com/air/foi/hr/mybook/PreporuceneKnjigeActivity.java
+++ myBook/app/src/main/java/com/air/foi/hr/mybook/PreporuceneKnjigeActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 
 
+import com.google.android.material.tabs.TabLayout;
 
 public class PreporuceneKnjigeActivity extends AppCompatActivity implements PreporuceneKnjigeFragment.OnFragmentInteractionListener {
 
@@ -14,6 +15,10 @@ public class PreporuceneKnjigeActivity extends AppCompatActivity implements Prep
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preporucene_knjige);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayoutPreporuke);
+        tabLayout.addTab(tabLayout.newTab().setText("Prijedlozi"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
     }
 
     @Override
