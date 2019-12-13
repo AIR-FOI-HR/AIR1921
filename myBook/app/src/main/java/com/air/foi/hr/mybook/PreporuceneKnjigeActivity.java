79da5324039f2e60869899e7ef7ac20e3c575735
Ana Guštek <ana.gustek@gmail.com>
2019-12-13 20:11:13 +0100

    Postavljeno da klasa PreporuceneKnjigeActivity implementira PreporuceneKnjigeFragment. #47 Implementirana potrebna metoda onFragmentInteraction.

--- myBook/app/src/main/java/com/air/foi/hr/mybook/PreporuceneKnjigeActivity.java
+++ myBook/app/src/main/java/com/air/foi/hr/mybook/PreporuceneKnjigeActivity.java
@@ -2,13 +2,22 @@ package com.air.foi.hr.mybook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import fragments.PreporuceneKnjigeFragment;
+import android.net.Uri;
 import android.os.Bundle;
 
-public class PreporuceneKnjigeActivity extends AppCompatActivity {
+
+
+public class PreporuceneKnjigeActivity extends AppCompatActivity implements PreporuceneKnjigeFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preporucene_knjige);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
