85b7719c733194dff561cca513be74f652872c75
Ana Guštek <ana.gustek@gmail.com>
2019-12-13 19:43:02 +0100

    Dodana klasa PrikazKnjigaAdapter. #47 Klasa je potrebna za tab sliding izbornik.

---
+++ myBook/app/src/main/java/com/air/foi/hr/mybook/adapters/PrikazKnjigaAdapter.java
@@ -0,0 +1,38 @@
+package com.air.foi.hr.mybook.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import fragments.PreporuceneKnjigeFragment;
+
+public class PrikazKnjigaAdapter extends FragmentStatePagerAdapter {
+
+    int sviTabovi;
+
+    public PrikazKnjigaAdapter(@NonNull FragmentManager fm, int brojTabova) {
+        super(fm);
+        this.sviTabovi=brojTabova;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                PreporuceneKnjigeFragment tabPreporucene = new PreporuceneKnjigeFragment();
+                return tabPreporucene;
+            //case 1: za knjige koje se trenutno citaju
+            default:
+                return null;
+
+
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return sviTabovi;
+    }
+}
