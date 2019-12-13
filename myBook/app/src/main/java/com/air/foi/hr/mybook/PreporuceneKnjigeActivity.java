e6e94bdbdbc642cbf87f430dcb3c7122cd8d556b
Ana Guštek <ana.gustek@gmail.com>
2019-12-13 20:29:59 +0100

    Dodana referenca na ViewPager i kreiran objekt za PrikazKnjigaAdapter. #47

--- myBook/app/src/main/java/com/air/foi/hr/mybook/PreporuceneKnjigeActivity.java
+++ myBook/app/src/main/java/com/air/foi/hr/mybook/PreporuceneKnjigeActivity.java
@@ -2,11 +2,13 @@ package com.air.foi.hr.mybook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.viewpager.widget.ViewPager;
 import fragments.PreporuceneKnjigeFragment;
 import android.net.Uri;
 import android.os.Bundle;
 
 
+import com.air.foi.hr.mybook.adapters.PrikazKnjigaAdapter;
 import com.google.android.material.tabs.TabLayout;
 
 public class PreporuceneKnjigeActivity extends AppCompatActivity implements PreporuceneKnjigeFragment.OnFragmentInteractionListener {
@@ -19,6 +21,11 @@ public class PreporuceneKnjigeActivity extends AppCompatActivity implements Prep
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayoutPreporuke);
         tabLayout.addTab(tabLayout.newTab().setText("Prijedlozi"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager=(ViewPager)findViewById(R.id.viewPagerPreporuke);
+        final PrikazKnjigaAdapter adapter=new PrikazKnjigaAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
     }
 
     @Override
