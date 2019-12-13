446240df4a8dcf28c59bbbbb977b9d45b6f54804
Ana Guštek <ana.gustek@gmail.com>
2019-12-13 20:35:06 +0100

    Implementirana metoda OnTabSelectedListener. #47

--- myBook/app/src/main/java/com/air/foi/hr/mybook/PreporuceneKnjigeActivity.java
+++ myBook/app/src/main/java/com/air/foi/hr/mybook/PreporuceneKnjigeActivity.java
@@ -26,6 +26,24 @@ public class PreporuceneKnjigeActivity extends AppCompatActivity implements Prep
         final PrikazKnjigaAdapter adapter=new PrikazKnjigaAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
         viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 
     @Override
