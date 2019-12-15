d10131517406b268a0ea58c13c9dc7be9f8b9506
bkavur <kavur.bojan@gmail.com>
2019-12-15 14:44:07 +0100

    Dodan algoritam za hashiranje lozinke. #38

--- myBook/app/src/main/java/com/air/foi/hr/mybook/LoginActivity.java
+++ myBook/app/src/main/java/com/air/foi/hr/mybook/LoginActivity.java
@@ -22,4 +22,25 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    // MD-5 hashiranje koje prima plain text lozinku, a vraća hashiranu vrijednost
+    private String hashpass(String pass) {
+        String generatedPassword;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(pass.getBytes());
+            byte[] bytes = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (Exception e)
+        {
+            Log.e("Exception", String.valueOf(e));
+            generatedPassword = "invalid";
+        }
+        return generatedPassword;
+    }
 }
