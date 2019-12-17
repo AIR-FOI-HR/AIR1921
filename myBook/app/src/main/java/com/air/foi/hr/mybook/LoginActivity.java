04956addbbc8bf652684a4ada8dc5f3ed123f4a4
bkavur <kavur.bojan@gmail.com>
2019-12-17 21:05:56 +0100

    Dohvaćeni elementi iz xml-a i dodano otvaranje registracije klikom na text view. #38

--- myBook/app/src/main/java/com/air/foi/hr/mybook/LoginActivity.java
+++ myBook/app/src/main/java/com/air/foi/hr/mybook/LoginActivity.java
@@ -1,16 +1,36 @@
 package com.air.foi.hr.mybook;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    private EditText email;
+    private  EditText password;
+    private ProgressBar progressBar;
+
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +39,23 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        TextView registration = findViewById(R.id.registration);
 
-    }
+        progressBar = findViewById(R.id.progressBar);
+
+        final FirebaseAuth firebaseAuth;
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Log.i(TAG, "Go to registration!");
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
 
     // MD-5 hashiranje koje prima plain text lozinku, a vraća hashiranu vrijednost
     private String hashpass(String pass) {
