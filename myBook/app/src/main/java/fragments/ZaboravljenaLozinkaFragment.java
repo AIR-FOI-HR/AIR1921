e826e7fd9262382e1d0070e60482f11f1c57d4c0
bkavur <kavur.bojan@gmail.com>
2019-12-19 20:44:24 +0100

    Dodano otvaranje forme za login klikom na text. #57

--- myBook/app/src/main/java/fragments/ZaboravljenaLozinkaFragment.java
+++ myBook/app/src/main/java/fragments/ZaboravljenaLozinkaFragment.java
@@ -1,23 +1,24 @@
 package fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.air.foi.hr.mybook.R;
 
 public class ZaboravljenaLozinkaFragment extends Fragment {
 
-    private static final String TAG = "ZaboravljenaLozinkaFragment";
+    private static final String TAG = "ZaboravLozinkaFragment";
 
     public ZaboravljenaLozinkaFragment() {
         // Required empty public constructor
@@ -35,5 +36,20 @@ public class ZaboravljenaLozinkaFragment extends Fragment {
 
         Toolbar toolbar = view.getRootView().findViewById(R.id.toolbar);
         toolbar.setTitle("Zaboravljena lozinka");
+
+        TextView login = view.findViewById(R.id.prijava_zaboravljena_lozinka);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Log.i(TAG, "Prijava clicked!");
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                LoginFragment loginFragment = new LoginFragment();
+                fragmentTransaction.hide(ZaboravljenaLozinkaFragment.this);
+                fragmentTransaction.replace(R.id.frameReg, loginFragment);
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
