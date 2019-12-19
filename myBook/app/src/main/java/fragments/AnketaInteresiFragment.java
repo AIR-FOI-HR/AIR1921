ef96e2f1f15e752d46b8baa01757616644a9aabb
Domagoj Kušter <dkuster@foi.hr>
2019-12-19 14:55:54 +0100

    Pozivanje ankete i stavljen gumb za spremanje. #5

--- myBook/app/src/main/java/fragments/AnketaInteresiFragment.java
+++ myBook/app/src/main/java/fragments/AnketaInteresiFragment.java
@@ -1,27 +1,50 @@
 package fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.air.foi.hr.mybook.LoginActivity;
 import com.air.foi.hr.mybook.R;
+import com.air.foi.hr.mybook.RegisterActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AnketaInteresiFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AnketaInteresiFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AnketaInteresiFragment extends Fragment {
 
+    private static final String TAG = "AnketaFragment";
 
+    public AnketaInteresiFragment(){
+
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_anketa_interesi, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        TODO: popravi error :D
+//        Toolbar toolbar = view.getRootView().findViewById(R.id.toolbar);
+//        toolbar.setTitle("Anketa");
+
+        Button saveForm = view.findViewById(R.id.buttonSpremiAnketu);
+        saveForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Log.i(TAG, "Prijava clicked!");
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+            }
+        });
+    }
 }
