1b46fd35577247c8088e48ba6f556a9b88d9d565
Domagoj Kušter <dkuster@foi.hr>
2019-12-31 13:45:46 +0100

    Promjenjen način implementacije dohvaćanja žanrova i spremanje interesa korisnika. #45

--- myBook/app/src/main/java/fragments/AnketaInteresiFragment.java
+++ myBook/app/src/main/java/fragments/AnketaInteresiFragment.java
@@ -5,13 +5,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckedTextView;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,28 +16,31 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.air.foi.hr.database.entities.Korisnik;
+import com.air.foi.hr.database.entities.KorisnikZanr;
 import com.air.foi.hr.database.entities.Zanr;
 import com.air.foi.hr.mybook.R;
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.firebase.ui.database.FirebaseListOptions;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class AnketaInteresiFragment extends Fragment {
 
     private static final String TAG = "AnketaFragment";
 
-    private FirebaseListAdapter adapter;
+    private DatabaseReference databaseZanr;
+    private DatabaseReference databaseKorisnikZanr;
+    private ArrayList<Zanr> genres = new ArrayList<>();
+    private ArrayList<Integer> checkedGenres = new ArrayList<>();
+    private Korisnik korisnik;
 
-
+    public AnketaInteresiFragment(Korisnik korisnik){
+        this.korisnik = korisnik;
+    }
     public AnketaInteresiFragment(){
     }
 
@@ -49,27 +49,26 @@ public class AnketaInteresiFragment extends Fragment {
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        adapter.startListening();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        adapter.stopListening();
-    }
-
-    @Override
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
+        databaseZanr = FirebaseDatabase.getInstance().getReference("zanr");
+        databaseKorisnikZanr = FirebaseDatabase.getInstance().getReference("korisnik_zanr");
+
         fillWithGenres();
+
 
         Button saveForm = view.findViewById(R.id.buttonSpremiAnketu);
         saveForm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
+                for (Integer integer: checkedGenres){
+                    KorisnikZanr korisnikZanr = new KorisnikZanr(korisnik.getKorime(), integer.toString());
+                    String id = databaseKorisnikZanr.push().getKey();
+                    databaseKorisnikZanr.child(id).setValue(korisnikZanr);
+                }
+
                 Log.i(TAG, "Save Form Anketa!");
                 FragmentManager fragmentManager = getFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -77,32 +76,54 @@ public class AnketaInteresiFragment extends Fragment {
                 fragmentTransaction.hide(AnketaInteresiFragment.this);
                 fragmentTransaction.replace(R.id.frameReg, loginFragment);
                 fragmentTransaction.commit();
-
-//              TODO: spremanje odabira
             }
         });
     }
 
     private void fillWithGenres() {
-        DatabaseReference databaseGenres = FirebaseDatabase.getInstance().getReference().child("zanr");
-
-        ListView listOfGenres = getView().findViewById(R.id.listOfGenres);
-        FirebaseListOptions<Zanr> options = new FirebaseListOptions.Builder<Zanr>()
-                .setLayout(R.layout.item_anketa)
-                .setLifecycleOwner(AnketaInteresiFragment.this)
-                .setQuery(databaseGenres, Zanr.class)
-                .build();
-
-        adapter = new FirebaseListAdapter(options) {
+        databaseZanr.addValueEventListener(new ValueEventListener() {
             @Override
-            protected void populateView(View v, Object model, int position) {
-                TextView genre = v.findViewById(R.id.zanr);
-
-                Zanr zanr = (Zanr) model;
-                genre.setText(zanr.getNaziv().toString());
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                genres.clear();
+                for (DataSnapshot data: dataSnapshot.getChildren()) {
+                    Zanr zanr = data.getValue(Zanr.class);
+                    genres.add(zanr);
+                    Log.i(TAG, genres.toString());
+                }
+                addCheckBox();
             }
-        };
 
-        listOfGenres.setAdapter(adapter);
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void addCheckBox(){
+        LinearLayout listOfGenres = getView().findViewById(R.id.listOfGenres);
+        for (int i = 0; i < genres.size(); i++){
+            final CheckBox checkBox = new CheckBox(getContext());
+            checkBox.setId(i);
+            checkBox.setText(genres.get(i).getNaziv());
+            checkBox.setTextSize(18);
+            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if(isChecked){
+                        checkedGenres.add(buttonView.getId());
+                        Log.i(TAG, "ID: " + buttonView.getId() + " Text: "+ buttonView.getText().toString());
+                        Log.i(TAG, checkedGenres.toString());
+                    }
+                    else {
+                            if (checkedGenres.contains(buttonView.getId())){
+                                checkedGenres.remove(Integer.valueOf(buttonView.getId()) );
+                            }
+                        Log.i(TAG, checkedGenres.toString());
+                    }
+                }
+            });
+            listOfGenres.addView(checkBox);
+        }
     }
 }
