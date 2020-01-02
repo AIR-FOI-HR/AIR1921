f836df0ff4e73bf936f21d2bfc5487c29d75cbb3
Ana Guštek <ana.gustek@gmail.com>
2020-01-02 19:03:52 +0100

    Prebačena je logika iz PreporuceneKnjigeActivity u PocetnaFragment. #58

--- myBook/app/src/main/java/fragments/PocetnaFragment.java
+++ myBook/app/src/main/java/fragments/PocetnaFragment.java
@@ -5,6 +5,7 @@ package fragments;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,6 +13,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.air.foi.hr.mybook.R;
+import com.air.foi.hr.mybook.adapters.ViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 
 public class PocetnaFragment extends Fragment {
@@ -20,5 +23,38 @@ public class PocetnaFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_pocetna,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TabLayout tabLayout = (TabLayout)view.findViewById(R.id.tabLayoutPreporuke);
+        tabLayout.addTab(tabLayout.newTab().setText("Prijedlozi"));
+        tabLayout.addTab(tabLayout.newTab().setText("Trenutno čitam"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager=(ViewPager)view.findViewById(R.id.viewPagerPreporuke);
+        final ViewPagerAdapter adapter=new ViewPagerAdapter(getChildFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 }
