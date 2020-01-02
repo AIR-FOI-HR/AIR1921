b8569b516d5cb2173da30cdfe50150d2bd90dba1
Ana Guštek <ana.gustek@gmail.com>
2020-01-02 21:29:05 +0100

    Dodana referenca na BottomNavigationView u klasi GlavniIzbornikActivity. #58

--- myBook/app/src/main/java/com/air/foi/hr/mybook/GlavniIzbornikActivity.java
+++ myBook/app/src/main/java/com/air/foi/hr/mybook/GlavniIzbornikActivity.java
@@ -3,6 +3,9 @@ package com.air.foi.hr.mybook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class GlavniIzbornikActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class GlavniIzbornikActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glavni_izbornik);
+        BottomNavigationView bottomNavigationView =findViewById(R.id.nav_view_glavna);
     }
 }
