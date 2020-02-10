7061e683a3aae6665fd0e3180d48b9bae0cbed7c
Ana Guštek <ana.gustek@gmail.com>
2020-02-10 17:27:15 +0100

    U fragmentu ProfilFragment je postavljeno otvaranje ostalih fragmenata na klik ikone. #32

--- myBook/app/src/main/java/hr/foi/air/mybook/fragments/ProfilFragment.java
+++ myBook/app/src/main/java/hr/foi/air/mybook/fragments/ProfilFragment.java
@@ -4,9 +4,13 @@ package hr.foi.air.mybook.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.fragment.app.FragmentManager;
 import hr.foi.air.mybook.R;
 
 import androidx.annotation.NonNull;
@@ -20,4 +24,38 @@ public class ProfilFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profil,container,false);
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                Fragment oznaceniFragment =null;
+
+                switch (menuItem.getItemId()){
+                    case R.id.nav_podaci:
+                        oznaceniFragment=new PodaciFragment();
+                        break;
+                    case R.id.nav_interesi:
+                        oznaceniFragment=new InteresiFragment();
+                        break;
+                    case R.id.nav_znacke:
+                        oznaceniFragment=new ZnackeFragment();
+                        break;
+                    case R.id.nav_statsitika:
+                        oznaceniFragment=new StatistikaFragment();
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + menuItem.getItemId());
+                }
+                FragmentManager fragmentManager = getChildFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.frame_profil, oznaceniFragment).commit();
+
+
+                return true;
+        }
+
+    };
+
+
 }
