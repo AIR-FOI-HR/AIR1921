bfcf788eb0b86da493a500d0edc75b6a7267a3d5
Ana Guštek <ana.gustek@gmail.com>
2020-02-14 13:28:58 +0100

    U fragmentu ZnackeFragment je definirana onViewCreated metoda gdje se poziva kreirani toolbar. #34

--- myBook/app/src/main/java/hr/foi/air/mybook/fragments/ZnackeFragment.java
+++ myBook/app/src/main/java/hr/foi/air/mybook/fragments/ZnackeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -17,5 +18,13 @@ public class ZnackeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_znacke,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Toolbar toolbar = view.getRootView().findViewById(R.id.toolbar_znacke);
+
     }
 }
