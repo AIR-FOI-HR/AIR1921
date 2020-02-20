85a6205ae4c2d688778d2cae9ddecdb222afae26
Ana Guštek <ana.gustek@gmail.com>
2020-02-20 19:32:25 +0100

    Kreiran je novi fragment za prikaz detalja knjige. #31

---
+++ myBook/app/src/main/java/hr/foi/air/mybook/fragments/DetaljiKnjigeFragment.java
@@ -0,0 +1,22 @@
+package hr.foi.air.mybook.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hr.foi.air.mybook.R;
+
+
+public class DetaljiKnjigeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_podaci,container,false);
+    }
+}
