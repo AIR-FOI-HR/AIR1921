377485fb5fa384b455f236937cea4e3b38814984
Ana Guštek <ana.gustek@gmail.com>
2020-02-21 13:09:25 +0100

    Definirana je onViewCreated metoda u fragmentu DetaljiKnjigeFragment. #31

--- myBook/app/src/main/java/hr/foi/air/mybook/fragments/DetaljiKnjigeFragment.java
+++ myBook/app/src/main/java/hr/foi/air/mybook/fragments/DetaljiKnjigeFragment.java
@@ -17,6 +17,6 @@ public class DetaljiKnjigeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_podaci,container,false);
+        return inflater.inflate(R.layout.fragment_detalji_knjige,container,false);
     }
 }
