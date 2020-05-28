7296e6d773c8d069bfe8014d5d3a49af7d3faf67
Ana Guštek <ana.gustek@gmail.com>
2020-05-28 15:50:45 +0200

    Dodane su varijable za prikaz detalja knjiga i povezane s elementima na formi. Dodan je konstruktor. #16

--- myBook/app/src/main/java/hr/foi/air/mybook/fragments/DetaljiKnjigeFragment.java
+++ myBook/app/src/main/java/hr/foi/air/mybook/fragments/DetaljiKnjigeFragment.java
@@ -6,17 +6,51 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import hr.foi.air.mybook.R;
 
 
 public class DetaljiKnjigeFragment extends Fragment {
+
+    private static final String TAG = "DetaljiKnjigeFragment";
+
+    private ImageView slikaKnjige;
+    private TextView nazivKnjige;
+    private TextView opisKnjige;
+    private TextView autorKnjige;
+    private RatingBar ocjenaKnjige;
+
+    public DetaljiKnjigeFragment() {
+        // Required empty public constructor
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_detalji_knjige,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        pocniCitati = view.findViewById(R.id.txt_pocni_citati);
+        slikaKnjige = view.findViewById(R.id.img_slika_knjige);
+        nazivKnjige = view.findViewById(R.id.txt_detalji_naziv);
+        autorKnjige = view.findViewById(R.id.txt_detalji_autor);
+        opisKnjige = view.findViewById(R.id.txt_detalji_opis);
+        ocjenaKnjige = view.findViewById(R.id.rating_bar_detalji_knjige);
+
+
     }
 }
