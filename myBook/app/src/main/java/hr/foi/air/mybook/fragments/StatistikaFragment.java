04dfc2be68a40b6212df3ec510ed11091ca30a6d
Ana Guštek <ana.gustek@gmail.com>
2020-06-03 14:07:46 +0200

    Dodana metoda za dohvaćanje korisničkog imena trenutnog korisnika. #29

--- myBook/app/src/main/java/hr/foi/air/mybook/fragments/StatistikaFragment.java
+++ myBook/app/src/main/java/hr/foi/air/mybook/fragments/StatistikaFragment.java
@@ -6,13 +6,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -51,9 +56,21 @@ public class StatistikaFragment extends Fragment {
    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
       return inflater.inflate(R.layout.fragment_statistika,container,false);
    }
+
+   @Override
+   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+      super.onViewCreated(view, savedInstanceState);
+
+      String firebaseKorisnik;
+
       databaseReferenceKnjiga = FirebaseDatabase.getInstance().getReference("knjiga");
       databaseReferenceCitanje = FirebaseDatabase.getInstance().getReference("citanje");
       databaseReferenceKorisnik = FirebaseDatabase.getInstance().getReference("korisnik");
+
+      FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+      firebaseKorisnik = firebaseAuth.getCurrentUser().getEmail();
+
+      dohvatiKorisnickoIme(firebaseKorisnik);
 
       recyclerView = view.findViewById(R.id.recycler_procitane);
       recyclerView.setHasFixedSize(true);
@@ -63,4 +80,31 @@ public class StatistikaFragment extends Fragment {
       recyclerView.setAdapter(adapter);
 
       brojProcitanihKnjiga = view.findViewById(R.id.txt_broj_procitanih);
+   }
+
+   private void dohvatiKorisnickoIme(final String korisnikMail) {
+
+      databaseReferenceKorisnik.addValueEventListener(new ValueEventListener() {
+         @Override
+         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            korisnici.clear();
+            for (DataSnapshot item : dataSnapshot.getChildren()) {
+               Korisnik korisnik = item.getValue(Korisnik.class);
+               korisnici.add(korisnik);
+            }
+            for (Korisnik korisnik : korisnici) {
+               Log.e(TAG, "pronađiKorisnickoIme: " + korisnik);
+               if (korisnik.getMail().equals(korisnikMail)) {
+                  korisnickoIme = korisnik.getKorime();
+                   Log.i(TAG, "Korisnik: "+korisnickoIme);
+               }
+            }
+         }
+         @Override
+         public void onCancelled(@NonNull DatabaseError databaseError) {
+
+         }
+      });
+   }
+
 }
