7e1570d4e69296d0d7933e6608a5a996239a67fd
Ana Guštek <ana.gustek@gmail.com>
2020-06-03 13:37:54 +0200

    Dodane reference za Firebase. #29

--- myBook/app/src/main/java/hr/foi/air/mybook/fragments/StatistikaFragment.java
+++ myBook/app/src/main/java/hr/foi/air/mybook/fragments/StatistikaFragment.java
@@ -10,12 +10,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import hr.foi.air.hr.database.entities.Citanje;
+import hr.foi.air.hr.database.entities.Knjiga;
+import hr.foi.air.hr.database.entities.Korisnik;
 import hr.foi.air.mybook.R;
 
 public class StatistikaFragment extends Fragment {
+
+   private DatabaseReference databaseReferenceKnjiga;
+   private DatabaseReference databaseReferenceCitanje;
+   private DatabaseReference databaseReferenceKorisnik;
    @Nullable
    @Override
    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
       return inflater.inflate(R.layout.fragment_statistika,container,false);
    }
+      databaseReferenceKnjiga = FirebaseDatabase.getInstance().getReference("knjiga");
+      databaseReferenceCitanje = FirebaseDatabase.getInstance().getReference("citanje");
+      databaseReferenceKorisnik = FirebaseDatabase.getInstance().getReference("korisnik");
 }
