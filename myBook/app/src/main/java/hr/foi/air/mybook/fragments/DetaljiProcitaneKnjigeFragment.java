50d92f7705fc5227a498badc77d5c99e8cd243ef
Ana Guštek <ana.gustek@gmail.com>
2020-06-05 12:19:42 +0200

    Implementirano spremanje datuma završetka čitanja knjige. #28

--- myBook/app/src/main/java/hr/foi/air/mybook/fragments/DetaljiProcitaneKnjigeFragment.java
+++ myBook/app/src/main/java/hr/foi/air/mybook/fragments/DetaljiProcitaneKnjigeFragment.java
@@ -21,8 +21,18 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
 import java.util.Calendar;
 
+import hr.foi.air.hr.database.entities.Citanje;
 import hr.foi.air.mybook.R;
 
 public class DetaljiProcitaneKnjigeFragment extends Fragment {
@@ -53,6 +63,7 @@ public class DetaljiProcitaneKnjigeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        databaseReferenceCitanje = FirebaseDatabase.getInstance().getReference("citanje");
 
         slikaKnjige=view.findViewById(R.id.img_slika_knjige_detalji_procitane);
         nazivKnjige=view.findViewById(R.id.txt_detalji_procitane_naziv);
@@ -95,6 +106,17 @@ public class DetaljiProcitaneKnjigeFragment extends Fragment {
             }
         };
 
+        Button spremiPromjene=view.findViewById(R.id.btn_procitana_spremi);
+        spremiPromjene.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "Spremi clicked ");
+
+                spremiPromjene();
+
+                Toast.makeText(getActivity(), "Promjene spremljene ", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void prikaziDetaljeProcitaneKnjige(){
@@ -115,4 +137,33 @@ public class DetaljiProcitaneKnjigeFragment extends Fragment {
         }
     }
 
+private void spremiPromjene(){
+
+    final String kraj=this.datumZavrsetka.getText().toString().trim();
+
+    databaseReferenceCitanje.orderByChild("korisnikKorime").equalTo(korisnik).addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            for (DataSnapshot item : dataSnapshot.getChildren()){
+                Citanje citanje=item.getValue(Citanje.class);
+                Log.i(TAG, citanje.toString());
+
+                if(citanje.getKnjigaIdKnjiga().equals(idKnjige)){
+                    Log.i(TAG, "CITANJE: "+citanje.toString());
+                    String citanjeKey=item.getKey();
+
+                    databaseReferenceCitanje.child(citanjeKey).child("datumKraja").setValue(kraj);
+
+                }
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    });
+
+
+}
 }
