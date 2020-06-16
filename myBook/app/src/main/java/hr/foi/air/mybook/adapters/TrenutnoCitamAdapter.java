3333f770df40e5d0b479e652dc6b1b280b0fca3c
Ana Guštek <ana.gustek@gmail.com>
2020-06-16 14:19:57 +0200

    U klasi TrenutnoCitamAdapter je definirana metoda ViewHolder. #12

--- myBook/app/src/main/java/hr/foi/air/mybook/adapters/TrenutnoCitamAdapter.java
+++ myBook/app/src/main/java/hr/foi/air/mybook/adapters/TrenutnoCitamAdapter.java
@@ -1,19 +1,39 @@
 package hr.foi.air.mybook.adapters;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import hr.foi.air.mybook.R;
+import hr.foi.air.mybook.objects.ProcitanaKnjigaObject;
 
 public class TrenutnoCitamAdapter extends RecyclerView.Adapter<TrenutnoCitamAdapter.ViewHolder>  {
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    private ArrayList<ProcitanaKnjigaObject> trenutnoCitam;
+    private Context context;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView nazivKnjige;
+        public TextView autorKnjige;
+        public TextView datumPocetka;
+        public ImageView slikaKnjige;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            nazivKnjige=itemView.findViewById(R.id.naslov_knjige_trenutno);
+            autorKnjige=itemView.findViewById(R.id.autor_knjige_trenutno);
+            datumPocetka=itemView.findViewById(R.id.pocetakcitanja_trenutno);
+            slikaKnjige=itemView.findViewById(R.id.slika_knjige_trenutno);
+
         }
     }
-    
+
     @NonNull
     @Override
     public TrenutnoCitamAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
