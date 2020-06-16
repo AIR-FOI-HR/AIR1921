8965e9b6a456ff32c35ec7e26dd46034d68891b6
Ana Guštek <ana.gustek@gmail.com>
2020-06-16 14:22:46 +0200

    Definirana je metoda onBindViewHolder u klasi TrenutnoCitamAdapter. #12

--- myBook/app/src/main/java/hr/foi/air/mybook/adapters/TrenutnoCitamAdapter.java
+++ myBook/app/src/main/java/hr/foi/air/mybook/adapters/TrenutnoCitamAdapter.java
@@ -1,17 +1,23 @@
 package hr.foi.air.mybook.adapters;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import hr.foi.air.mybook.R;
+import hr.foi.air.mybook.fragments.DetaljiProcitaneKnjigeFragment;
 import hr.foi.air.mybook.objects.ProcitanaKnjigaObject;
 
 public class TrenutnoCitamAdapter extends RecyclerView.Adapter<TrenutnoCitamAdapter.ViewHolder>  {
@@ -49,12 +55,46 @@ public class TrenutnoCitamAdapter extends RecyclerView.Adapter<TrenutnoCitamAdap
 
     @Override
     public void onBindViewHolder(@NonNull TrenutnoCitamAdapter.ViewHolder holder, int position) {
+        final ProcitanaKnjigaObject knjiga = trenutnoCitam.get(position);
+        Log.i("MSG", knjiga.getNaziv());
 
+        holder.nazivKnjige.setText(knjiga.getNaziv());
+        holder.autorKnjige.setText(knjiga.getAutor());
+        holder.datumPocetka.setText(knjiga.getDatumPocetka());
+        Picasso.with(context)
+                .load(knjiga.getUrlSlike())
+                .into(holder.slikaKnjige);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DetaljiProcitaneKnjigeFragment detaljiProcitaneKnjigeFragment=new DetaljiProcitaneKnjigeFragment();
+
+                Bundle arguments = new Bundle();
+                arguments.putString("id", knjiga.getIdKnjiga());
+                arguments.putString("naziv", knjiga.getNaziv());
+                arguments.putString("autor", knjiga.getAutor());
+                arguments.putString("slika", knjiga.getUrlSlike());
+                arguments.putString("datumPocetka", knjiga.getDatumPocetka());
+                arguments.putString("datumZavrsetka", knjiga.getDatumKraja());
+                arguments.putFloat("ocjena",knjiga.getOcjena());
+                arguments.putString("komentar",knjiga.getKomentar());
+                arguments.putString("korisnik",knjiga.getKorisnikKorime());
+                detaljiProcitaneKnjigeFragment.setArguments(arguments);
+
+                AppCompatActivity activity=(AppCompatActivity)view.getContext();
+                activity.getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame_izbornik, detaljiProcitaneKnjigeFragment)
+                        .addToBackStack("")
+                        .commit();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return trenutnoCitam.size();
     }
 
 }
