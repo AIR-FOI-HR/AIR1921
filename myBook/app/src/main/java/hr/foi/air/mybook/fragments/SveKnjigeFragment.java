5b828547317e9d2585edca1b4919f570f9626089
bkavur <bojkavur@foi.hr>
2020-06-20 19:16:35 +0200

    Implementirano filtriranje prema žanru. #14

--- myBook/app/src/main/java/hr/foi/air/mybook/fragments/SveKnjigeFragment.java
+++ myBook/app/src/main/java/hr/foi/air/mybook/fragments/SveKnjigeFragment.java
@@ -2,19 +2,26 @@ package hr.foi.air.mybook.fragments;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import hr.foi.air.hr.database.entities.Citanje;
+import hr.foi.air.hr.database.entities.Knjiga;
 import hr.foi.air.hr.database.entities.Zanr;
+import hr.foi.air.hr.database.entities.ZanrKnjiga;
 import hr.foi.air.mybook.R;
 import hr.foi.air.mybook.objects.BookListObject;
+import hr.foi.air.mybook.recyclerview.BookRecyclerAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -33,6 +40,7 @@ public class SveKnjigeFragment extends Fragment {
     private DatabaseReference databaseReference;
     private DatabaseReference databaseReferenceNew;
     private DatabaseReference databaseZanrovi;
+    private DatabaseReference databaseZanrKnjige;
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
@@ -56,6 +64,10 @@ public class SveKnjigeFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        databaseReference = FirebaseDatabase.getInstance().getReference("knjiga");
+        databaseReferenceNew = FirebaseDatabase.getInstance().getReference("citanje");
+        databaseZanrKnjige = FirebaseDatabase.getInstance().getReference("zanr_knjige");
+
         databaseZanrovi = FirebaseDatabase.getInstance().getReference("zanr");
 
         zanr = view.findViewById(R.id.zanr);
@@ -65,6 +77,27 @@ public class SveKnjigeFragment extends Fragment {
                 android.R.layout.simple_spinner_dropdown_item, spinnerDataList);
         zanr.setAdapter(zanrAdapter);
         retrieveData();
+
+        recyclerView = view.findViewById(R.id.recycler_rezultat_filtriranja);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        adapter = new BookRecyclerAdapter(view.getContext(), books);
+
+        recyclerView.setAdapter(adapter);
+
+        zanr.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Zanr selectedZanr = (Zanr) zanr.getSelectedItem();
+                Log.i(TAG, "Selected zanr id = " + selectedZanr.getIdZanr() + ", name = " + selectedZanr.getNaziv());
+                Log.i(TAG, "Retrieve books from DB");
+                getAllBooks(selectedZanr);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
 
     private void retrieveData() {
@@ -83,5 +116,91 @@ public class SveKnjigeFragment extends Fragment {
 
             }
         });
+    }
+
+    private void getAllBooks(Zanr filter) {
+        databaseZanrKnjige.orderByChild("zanrIdZanr").equalTo(filter.getIdZanr()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot zanrKnjigaData : dataSnapshot.getChildren()) {
+                    final ZanrKnjiga zanrKnjiga = zanrKnjigaData.getValue(ZanrKnjiga.class);
+                    Log.i(TAG, "Knjige zanra => knjigaid =  " + zanrKnjiga.getKnjigaIdKnjiga() + ", zanrId = " + zanrKnjiga.getZanrIdZanr());
+
+                    //TODO: knjige iz žanra i izračun...
+                    databaseReference.orderByChild("idKnjiga").equalTo(zanrKnjiga.getKnjigaIdKnjiga()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            books.clear();
+                            for (DataSnapshot data : dataSnapshot.getChildren()) {
+                                final Knjiga book = data.getValue(Knjiga.class);
+                                Log.i(TAG, book.toString());
+
+                                final ArrayList<Citanje> citanjeArrayList = new ArrayList<>();
+
+                                databaseReferenceNew.addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        citanjeArrayList.clear();
+                                        for (DataSnapshot data : dataSnapshot.getChildren()) {
+                                            Citanje citanje = data.getValue(Citanje.class);
+                                            if (citanje.getKnjigaIdKnjiga().equals(book.getIdKnjiga())) {
+                                                Log.i(TAG, citanje.toString());
+                                                citanjeArrayList.add(citanje);
+                                            }
+                                        }
+                                        calculateRating(citanjeArrayList, book);
+                                        showData(books);
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                                    }
+                                });
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+    }
+
+    private void calculateRating(ArrayList<Citanje> citanjeArrayList, Knjiga book) {
+        BookListObject bookListObject = new BookListObject();
+
+        bookListObject.setIdKnjiga(book.getIdKnjiga());
+        bookListObject.setAutor(book.getAutor());
+        bookListObject.setNaziv(book.getNaziv());
+        bookListObject.setSazetak(book.getSazetak());
+        bookListObject.setUrlSlike(book.getUrlSlike());
+
+        if (citanjeArrayList.isEmpty()) {
+            Log.i(TAG, "No rating");
+            bookListObject.setOcjena(0);
+        } else {
+            float sum = 0;
+            for (Citanje citanje : citanjeArrayList) {
+                sum += citanje.getOcjena();
+                Log.i(TAG, "Current sum: " + sum);
+            }
+            float rating = sum / citanjeArrayList.size();
+            Log.i(TAG, "Rating: " + rating);
+            bookListObject.setOcjena(rating);
+        }
+
+        books.add(bookListObject);
+    }
+
+    private void showData(ArrayList<BookListObject> books) {
+        adapter = new BookRecyclerAdapter(getContext(), books);
+
+        recyclerView.setAdapter(adapter);
     }
 }
